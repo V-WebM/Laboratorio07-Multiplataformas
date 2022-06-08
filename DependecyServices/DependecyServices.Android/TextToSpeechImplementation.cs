836729f7728166408c07c3a63a8bb5ef16b991d0
Ventura <marlenp363@gmail.com>
2022-06-08 10:27:40 -0500

    primer commit

---
+++ DependecyServices/DependecyServices.Android/TextToSpeechImplementation.cs
@@ -0,0 +1,46 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Speech.Tts;
+using Xamarin.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DependecyServices.Droid;
+
+[assembly: Dependency(typeof(TextToSpeechImplementation))]
+namespace DependecyServices.Droid
+{
+    public class TextToSpeechImplementation : Java.Lang.Object, ITextToSpeech, TextToSpeech.IOnInitListener
+    {
+        TextToSpeech speaker;
+        string toSpeak;
+
+        public void Speak(string text)
+        {
+            toSpeak = text;
+            if (speaker == null)
+            {
+                speaker = new TextToSpeech(Forms.Context, this);
+            }
+            else
+            {
+                speaker.Speak(toSpeak, QueueMode.Flush, null, null);
+            }
+        }
+
+        #region InOnInitListener implementation
+        public void OnInit(OperationResult status)
+        {
+            if (status.Equals(OperationResult.Success))
+            {
+                speaker.Speak(toSpeak, QueueMode.Flush, null, null);
+            }
+        }
+        #endregion
+    }
+}
---
+++ DependecyServices/DependecyServices/ITextToSpeech.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DependecyServices
+{
+    internal interface ITextToSpeech
+    {
+        void Speak(string text);
+    }
+}
